f1a616ec7f20873ef9eea5df09cb900251b2a74f
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2022-12-13 16:06:39 +0500

    init

---
+++ src/js/files/fullpage.js
@@ -0,0 +1,8 @@
+// // import fullpage from "../libs/fullpage.js";
+// import "../libs/fullpage.js";
+
+// import "../../scss/libs/fullpage.min.scss";
+
+// window.addEventListener("load", function () {
+//   console.log(new fullpage());
+// })
---
+++ src/js/files/script.js
@@ -0,0 +1,3 @@
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
